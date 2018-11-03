5175231967f92e4c2753e02be86be95b7feb89c3
Alex Cho <alexsjcho@gmail.com>
2018-11-02 20:40:35 -0700

    final upload to github

--- website_blocker.py
+++ website_blocker.py
@@ -2,14 +2,14 @@ import time
 from datetime import datetime as dt
 
 hosts_temp= "hosts"
-hosts_path=r"D:\Dropbox\pp\block_websites\Demo\hosts"
+hosts_path="/etc/hosts"
 redirect= "127.0.0.1"
 website_list=["www.sonic.com","sonic.com"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,16) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,17):
         print("Working hours...")
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content=file.read()
             print(content)
             for website in website_list:
@@ -19,10 +19,12 @@ while True:
                     file.write(redirect+""+ website+"\n")
 
     else:
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content =file.readlines()
+            file.seek()
             for line in content:
                 if not any(website in line for website in website_list):
                     file.write(line)
+                file.truncate()
         print("Fun hours!!!")
     time.sleep(5)
